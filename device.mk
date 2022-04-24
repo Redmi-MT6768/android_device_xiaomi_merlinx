7ae96071a17108e5aca44cee9d8b41918e72b8ed
Sarthak Roy <sarthakroy2002@gmail.com>
2022-04-24 10:08:03 +0300

    merlinx: Dont copy com.nxp.mifare permission
    - We dont need it.
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Ic9b0de75ba7c8765d282785c0aef8572446321cc

--- device.mk
+++ device.mk
@@ -33,8 +33,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_merlinnfc/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_merlinnfc/android.hardware.nfc.hcef.xml \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_merlinnfc/android.hardware.nfc.uicc.xml \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_merlinnfc/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_merlinnfc/com.nxp.mifare.xml
+    frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_merlinnfc/com.android.nfc_extras.xml
 
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
